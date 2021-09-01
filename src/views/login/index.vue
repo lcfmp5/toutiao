ddf426f57c409e3621342e3c88b567e87e374a5e
lcfmp5 <65054301@qq.com>
2021-09-02 01:46:41 +0800

    first commit

---
+++ src/views/login/index.vue
@@ -0,0 +1,162 @@
+<template>
+  <div class="login-container">
+    <!-- 导航栏 -->
+    <van-nav-bar class="page-nav-bar"
+                 type="pr"
+                 title="登录">
+      <van-icon slot="left"
+                name="cross"
+                @click="$router.back()" />
+    </van-nav-bar>
+    <!-- /导航栏 -->
+
+    <!-- 登录表单 -->
+    <van-form @submit="onSubmit"
+              ref="loginForm">
+      <van-field v-model="user.mobile"
+                 lable="手机号"
+                 placeholder="请输入手机号"
+                 name="mobile"
+                 :rules="userFormRules.mobile"
+                 maxlength="11">
+        <i slot="left-icon"
+           class="toutiao toutiao-shouji"></i>
+      </van-field>
+      <van-field v-model="user.code"
+                 type="number"
+                 lable="验证码"
+                 name="code"
+                 placeholder="请输入验证码"
+                 :rules="userFormRules.code"
+                 maxlength="6">
+        <i slot="left-icon"
+           class="toutiao toutiao-yanzhengma"></i>
+        <template #button>
+          <van-count-down v-if="isCountDownShow"
+                          :time="1000*60"
+                          format="ss s"
+                          @finish="isCountDownShow = false">
+          </van-count-down>
+          <van-button v-else
+                      class="send-sms-btn"
+                      round
+                      size="small"
+                      type="default"
+                      native-type="button"
+                      @click="onSendSms">发送验证码</van-button>
+        </template>
+      </van-field>
+      <div class="login-btn-wrap">
+        <van-button class="login-btn"
+                    block
+                    type="info"
+                    native-type="submit">
+          登录
+        </van-button>
+      </div>
+    </van-form>
+    <!-- /登录表单 -->
+  </div>
+</template>
+
+<script>
+import { login, sendSms } from '@/api/user.js'
+export default {
+  name: 'LoginPage',
+  data () {
+    return {
+      user: {
+        mobile: '',
+        code: ''
+      },
+      userFormRules: {
+        mobile: [{
+          required: true,
+          message: '手机号不能为空'
+        }, {
+          pattern: /^1[3|5|7|8]\d{9}$/,
+          message: '手机号格式错误'
+        }],
+        code: [{
+          required: true,
+          message: '验证码不能为空'
+        }, {
+          pattern: /^\d{6}$/,
+          message: '验证码格式错误'
+        }]
+      },
+      isCountDownShow: false
+    }
+  },
+  methods: {
+    async onSubmit () {
+      const user = this.user
+      this.$toast.loading({
+        message: '登录中...',
+        forbidClick: true, // 禁用背景点击
+        duration: 0 // 持续时间，默认 2000，0 表示持续展示不关闭
+      })
+      // todo 表单验证
+
+      try {
+        const { data } = await login(user)
+        this.$store.commit('setUser', data.data)
+        console.log('登录成功', { data })
+        this.$toast.success('登录成功')
+        this.$router.back()
+      } catch (err) {
+        if (err.response.status === 400) {
+          this.$toast.fail('手机号或验证码错误')
+        } else {
+          this.$toast.fail('登录失败，请稍后重试')
+        }
+      }
+    },
+    async onSendSms () {
+      try {
+        await this.$refs.loginForm.validate('mobile')
+      } catch (err) {
+        return console.log('验证失败', err)
+      }
+      this.isCountDownShow = true
+      try {
+        await sendSms(this.user.mobile)
+        this.$toast('发送成功')
+      } catch (err) {
+        this.isCountDownShow = false
+        if (err.response.status === 429) {
+          this.$toast('发送太过频繁，请稍后重试')
+        } else {
+          this.$toast('发送失败，请稍后重试')
+        }
+      }
+    }
+
+  }
+}
+</script>
+
+<style scoped lang="less">
+.login-container {
+  .toutiao {
+    font-size: 37px;
+  }
+
+  .send-sms-btn {
+    width: 152px;
+    height: 46px;
+    line-height: 46px;
+    background-color: #ededed;
+    font-size: 22px;
+    color: #666;
+  }
+
+  .login-btn-wrap {
+    padding: 53px 33px;
+    .login-btn {
+      background-color: #6db4fb;
+      border: none;
+    }
+  }
+}
+</style>
---
+++ src/views/qa/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    qa
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
